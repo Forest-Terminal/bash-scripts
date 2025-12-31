8fc666f15036263dc8040b3677b7f37a4c7e2b44
Forest Terminal <138257699+Forest-Terminal@users.noreply.github.com>
2025-12-31 08:20:36 -0600

    Simplify wallpaper selection in wallpaper.sh
    Removed theme-based wallpaper selection logic.

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,12 +2,4 @@
 
 wallpaper=$(find ~/wal/paper/ -type f -print | fzf --preview '/home/forest-terminal/wal/preview.sh {}')
 
-if [[ $(grep -c "catppuccin" /home/forest-terminal/.config/colors/theme.css) -eq 1 ]]; then
-  wallpaper=${wallpaper//paper/catppuccin}
-elif [[ $(grep -c "rose" /home/forest-terminal/.config/colors/theme.css) -eq 1 ]]; then
-  wallpaper=${wallpaper//paper/rosepine}
-elif [[ $(grep -c "gruvbox" /home/forest-terminal/.config/colors/theme.css) -eq 1 ]]; then
-  wallpaper=${wallpaper//paper/gruvbox}
-fi
-
 swww img ${wallpaper} --transition-type wipe
